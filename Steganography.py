f6515ed94288ed7d4986141c3c6eb44a326a553e
Schifti <M.J.Schifter@ncl.ac.uk>
2024-03-14 16:03:22 +0000

    Data now hidden in pixel values changes not plain overwrites

--- Steganography.py
+++ Steganography.py
@@ -11,15 +11,25 @@ def encode(image, msg, seed):
     for char in msg:
         x, y = random.randint(0, width - 1), random.randint(0, height - 1)
         r, g, b, o = pix[x, y]
-        if c == 0:
-            pix[x, y] = ord(char), g, b, o
-            c = c + 1
-        elif c == 1:
-            pix[x, y] = r, ord(char), b, o
-            c = c + 1
-        else:
-            pix[x, y] = r, g, ord(char), o
-            c = 0
+        if pix[x, y] == pix[x + 1, y]:
+            if c == 0:
+                if r + ord(char) > 255:
+                    pix[x, y] = r + ord(char)-256, g, b, o
+                else:
+                    pix[x, y] = r + ord(char), g, b, o
+                c = c + 1
+            elif c == 1:
+                if g + ord(char) > 255:
+                    pix[x, y] = r, g + ord(char)-256, b, o
+                else:
+                    pix[x, y] = r, g + ord(char), b, o
+                c = c + 1
+            else:
+                if b + ord(char) > 255:
+                    pix[x, y] = r, g, b + ord(char)-256, o
+                else:
+                    pix[x, y] = r, g, b + ord(char), o
+                c = 0
     pix[random.randint(0, width - 1), random.randint(0, height - 1)] = 0, 255, 174, 255
     im.save('test.png')
     return
@@ -38,14 +48,24 @@ def decode(image, seed):
         r, g, b, o = pix[x, y]
         if pix[x, y] == (0, 255, 174, 255):
             return msg
+        kr, kg, kb, ko = pix[x + 1, y]
         if c == 0:
-            msg = msg + chr(r)
+            if r - kr < -1:
+                msg = msg + chr(r - kr + 256)
+            else:
+                msg = msg + chr(r - kr)
             c = c + 1
         elif c == 1:
-            msg = msg + chr(g)
+            if g - kg < -1:
+                msg = msg + chr(g - kg + 256)
+            else:
+                msg = msg + chr(g - kg)
             c = c + 1
         else:
-            msg = msg + chr(b)
+            if b - kb < -1:
+                msg = msg + chr(b - kb + 256)
+            else:
+                msg = msg + chr(b - kb)
             c = 0
 
 
@@ -54,7 +74,7 @@ def main():
     while not end:
         choice = input('Would you like to: \n(e)ncode \n(d)ecode \n(q)uit \n')
         if choice == 'e':
-            encode('pfp.png', "Hello World I hate everyone!", 573909672895873)
+            encode('pfp.png', "Hello World", 573909672895873)
             print('Message Encoded')
         elif choice == 'd':
             print('Message:', decode('test.png', 573909672895873))
@@ -65,4 +85,5 @@ def main():
     return
 
 
-main()
+encode('pfp.png', "Hello World", 573909672895873)
+print('Message:', decode('test.png', 573909672895873))
