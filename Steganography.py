2759ca37915e94b884de4cd784bc7a8243794f29
schifti <martinjschifter@gmail.com>
2024-03-15 00:52:19 +0000

    The current bug is in the pixel matching system, not fixed yet

--- Steganography.py
+++ Steganography.py
@@ -9,9 +9,12 @@ def encode(image, msg, seed):
     c = 0
     random.seed(seed)
     for char in msg:
+
+        print(char)
         x, y = random.randint(0, width - 2), random.randint(0, height - 2)
         r, g, b, o = pix[x, y]
-        if pix[x, y] == pix[x + 1, y]:
+        print(pix[x, y], pix[x+1, y])
+        if pix[x, y] == pix[x + 1, y]:  # issue is here!
             print(c, char)
             if c == 0:
                 if r + ord(char) > 255:
@@ -19,22 +22,25 @@ def encode(image, msg, seed):
                 else:
                     pix[x, y] = r + ord(char), g, b, o
                 c = c + 1
+                pog = r + ord(char)
             elif c == 1:
                 if g + ord(char) > 255:
                     pix[x, y] = r, g + ord(char) - 256, b, o
                 else:
                     pix[x, y] = r, g + ord(char), b, o
                 c = c + 1
+                pog = g + ord(char)
             elif c == 2:
                 if b + ord(char) > 255:
                     pix[x, y] = r, g, b + ord(char) - 256, o
                 else:
-                    print(b, ord(char), b + ord(char), char, 'colour b')
                     pix[x, y] = r, g, b + ord(char), o
-                    print(pix[x,y])
+                pog = b + ord(char)
                 c = 0
             else:
                 print('WTF')
+        else:
+            print('no dupe')
     pix[random.randint(0, width - 2), random.randint(0, height - 2)] = 0, 255, 174, 255
     im.save('test.png')
     return
@@ -56,24 +62,20 @@ def decode(image, seed):
         kr, kg, kb, ko = pix[x + 1, y]
         if c == 0:
             if r - kr < 0:
-                #print(r, kr, r - kr, r - kr + 256, 'r')
                 msg = msg + chr(r - kr + 256)
             else:
                 msg = msg + chr(r - kr)
             c = c + 1
         elif c == 1:
             if g - kg < 0:
-                #print(g, kg, g - kg, g - kg + 256, 'g')
                 msg = msg + chr(g - kg + 256)
             else:
                 msg = msg + chr(g - kg)
             c = c + 1
         else:
             if b - kb < 0:
-                print(b, kb, b - kb, b - kb + 256, 'b')
                 msg = msg + chr(b - kb + 256)
             else:
-                print(b, kb, b - kb, 'b', chr(b - kb), pix[x,y])
                 msg = msg + chr(b - kb)
             c = 0
 
